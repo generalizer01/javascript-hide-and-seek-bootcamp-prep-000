5dc05ecae71a0cfb9dfc30559e50ec8ac5b268af
guymarsden01@gmail.com <guymarsden01@gmail.com>
2018-05-13 09:04:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   let ranks = document.querySelectorAll('ul.ranked-list li')
+  for (let i=0;i<ranks.length;i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML)+1
+  }
 }
