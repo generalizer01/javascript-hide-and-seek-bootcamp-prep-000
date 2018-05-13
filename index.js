f99d9db052a97219d943c17e78e956de0a5bf44c
guymarsden01@gmail.com <guymarsden01@gmail.com>
2018-05-13 08:53:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function getFirstSelector(selector) {
 }
 
 function nestedTarget() {
-  let out = document.getElementById('nested').querySelector('.target');
-  return out;
+  return document.getElementById('nested').querySelector('.target');
+
 }
